e44919e2d1a4f829122a4da2e3c7c4e6dacdd879
libin <libin@adbright.cn>
2020-12-28 23:33:48 +0800

    µÚһ´ÎÌύ

---
+++ cloudProviderPay/src/main/java/com/dabin/springcloud/entities/order.java
@@ -0,0 +1,11 @@
+package com.dabin.springcloud.entities;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName:order
+ * @author: dabin
+ * @date: 2020/12/2221:32
+ */
+public class order implements Serializable {
+}
