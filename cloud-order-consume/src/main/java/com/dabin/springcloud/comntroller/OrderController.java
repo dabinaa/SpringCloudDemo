0470fce7a41560cd6f1b62078b62446e1a70e7fd
libin <libin@adbright.cn>
2021-01-06 23:50:43 +0800

    增加一些æµ负载均衡的总结

--- cloud-order-consume/src/main/java/com/dabin/springcloud/comntroller/OrderController.java
+++ cloud-order-consume/src/main/java/com/dabin/springcloud/comntroller/OrderController.java
@@ -4,6 +4,9 @@ import com.dabin.springcloud.entities.CommonResult;
 import com.dabin.springcloud.entities.Order;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -31,13 +34,22 @@ public class OrderController {
         return restTemplate.postForObject(PAY_URL + "dabin", null, CommonResult.class);
     }
 
+
+    @GetMapping("comsumer/getForEntity/{id}")
+    public CommonResult<Order> getPayMent(@PathVariable("id") Integer id) {
+//        return restTemplate.postForObject(PAY_URL + "dabin", null, CommonResult.class);
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAY_URL + "dabin", CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            return forEntity.getBody();
+        }
+        return new CommonResult<>(444, "shibai");
+    }
+
     private Object getDisCovery() {
         List<String> services = discoveryClient.getServices();
         System.out.println(services);
         List<ServiceInstance> instances = discoveryClient.getInstances("");
         System.out.println(instances);
-
-
         return discoveryClient;
     }
 
